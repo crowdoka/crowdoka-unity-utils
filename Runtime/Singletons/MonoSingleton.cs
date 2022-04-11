807c5d3a37acb9fd6c09fd06e031e07bc0431686
Vincent Ordioni <vincent.ordioni@gmail.com>
2022-04-11 22:04:06 +0200

    Initial commit

---
+++ Runtime/Singletons/MonoSingleton.cs
@@ -0,0 +1,28 @@
+﻿namespace Crowdoka.Utils.Singletons
+{
+    using System;
+    using UnityEngine;
+
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static T singletonInstance;
+
+        protected void InitInstance(T instance)
+        {
+			singletonInstance = instance ?? throw new Exception($"Instance of type {nameof(T)} cannot be null");
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (singletonInstance == null)
+                {
+                    throw new Exception($"{nameof(MonoSingleton<T>)} used before initialization. Please use {nameof(InitInstance)} before using the singleton instance");
+                }
+
+                return singletonInstance;
+            }
+        }
+    }
+}
---
+++ Runtime/Utils/EnumUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace Crowdoka.Utils.Utils
+{
+    using System;
+    using System.Linq;
+
+    public static class EnumUtils
+    {
+        public static T[] GetValues<T>()
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>().ToArray();
+        }
+    }
+}
